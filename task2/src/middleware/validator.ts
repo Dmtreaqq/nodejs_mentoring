a48faad922bde7be5127aa099a636731b25c386f
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-01-20 10:22:20 +0200

    MNTR: develop init

--- task2/src/middleware/validator.ts
+++
@@ -1,8 +0,0 @@
-import { ContainerTypes, createValidator, ValidatedRequestSchema } from 'express-joi-validation';
-import { User } from '../types/User';
-
-export const validator = createValidator();
-
-export interface UserRequestSchema extends ValidatedRequestSchema {
-    [ContainerTypes.Body]: User
-}
--- task2/src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './User';
-
-declare global {
-    namespace Express {
-        export interface Request {
-            user: User
-        }
-    }
-}
