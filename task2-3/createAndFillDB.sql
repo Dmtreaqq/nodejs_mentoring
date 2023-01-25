2d0df965b30f64c8aa38872285c780921b296df7
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-01-25 22:04:46 +0200

    MNTR: task 4.2, implement many-to-many relationship

--- task2-3/createAndFillDB.sql
+++ task2-3/createAndFillDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS user_group;
 
 CREATE TABLE users (
 	id text PRIMARY KEY,
@@ -15,12 +16,20 @@ CREATE TABLE groups (
 	permissions text []
 );
 
+CREATE TABLE user_group (
+  user_id text,
+  group_id text,
+  PRIMARY KEY (user_id, group_id),
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id),
+  CONSTRAINT fk_group FOREIGN KEY(group_id) REFERENCES groups(id) ON DELETE CASCADE
+);
+
 INSERT INTO
 	users(id, login, password, age, is_deleted)
 VALUES
-	(gen_random_uuid(), 'Dmytro', '1337', 23, false),
-	(gen_random_uuid(), 'Pavlo', '1444', 24, false),
-	(gen_random_uuid(), 'Oleh', '98765', 18, false),
-	(gen_random_uuid(), 'Zekora', 'qwerty', 27, false),
+	(gen_random_uuid(), 'Dmytro', 'q1337', 23, false),
+	(gen_random_uuid(), 'Pavlo', 'q1444', 24, false),
+	(gen_random_uuid(), 'Oleh', 'q98765', 18, false),
+	(gen_random_uuid(), 'Zekora', '123qwerty', 27, false),
 	(gen_random_uuid(), 'Artem', 'qwerty123', 44, false),
 	(gen_random_uuid(), 'Daniel', 'steelseries28', 20, true);
