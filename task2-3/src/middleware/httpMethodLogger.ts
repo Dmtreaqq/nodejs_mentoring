65773beca7cd5711b0cadd63c9eee13cad499eb0
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-02-17 13:24:03 +0200

    MNTR: fix response code and added response time to task 5.1

--- task2-3/src/middleware/httpMethodLogger.ts
+++ task2-3/src/middleware/httpMethodLogger.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
+import logger from './logger';
 
 
 export const httpMethodLogger = (req: Request, res: Response, next: NextFunction) => {
-    const { method, url } = req;
-    const { statusCode } = res;
+    const startRequestTime = Date.now()
 
-    const loggerString = `${method} ${url} ${statusCode}`;
-    console.log(loggerString);
+    res.on('finish', () => {
+        const responseTime = Date.now() - startRequestTime;
+        const { method, originalUrl } = req;
+        const { statusCode } = res;
+
+        logger.info(`${method} ${originalUrl} ${statusCode} - ~${responseTime} ms`)
+    })
+
     next();
 };
